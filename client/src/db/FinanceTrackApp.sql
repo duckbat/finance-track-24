c33077a8f0f6f5e8a5ac6a78020c0d20b4533915
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-02-19 13:20:14 +0200

    db update

--- client/src/db/FinanceTrackApp.sql
+++ client/src/db/FinanceTrackApp.sql
@@ -5,18 +5,30 @@ USE FinanceTrackApp;
 
 -- Create the tables
 
+-- Userlevel table (Moderator, User, Guest, etc.)
+CREATE TABLE UserLevels (
+    level_id INT AUTO_INCREMENT PRIMARY KEY,
+    level_name VARCHAR(50) NOT NULL,
+);
+
 -- Users table
 CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    profile_pic VARCHAR(255),
+    FOREIGN KEY (user_level_id) REFERENCES UserLevels(level_id)
 );
 
 -- Transactions table
+CREATE TABLE Transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
 
--- Catory table
+);
+
+-- Category table
 CREATE TABLE
 
 -- Friends table
@@ -26,3 +38,5 @@ CREATE TABLE
 -- Likes table
 
 
+
+-- Sample data
